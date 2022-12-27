6d1a7c7449a9468d1ec74fed47c029f094df626f
nabiursiddique <nabiursiddique01@gmail.com>
2022-12-28 00:47:07 +0600

    Appointment section is completes

---
+++ src/pages/Home/Testimonial/Testimonial.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Testimonial = () => {
+    return (
+        <section>
+            <div>
+                <div>
+                    <h4 className="text-xl text-primary font-bold">Testimonial</h4>
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </section>
+    );
+};
+
+export default Testimonial;
