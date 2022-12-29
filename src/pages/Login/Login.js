a1f33d19c004d1f84a79509bb4b2eb2c25db83b2
nabiursiddique <nabiursiddique01@gmail.com>
2022-12-29 20:49:25 +0600

    Login page is completed

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const handleLogin = data => console.log(data)
     return (
-        <div>
-            <h2>This is login page</h2>
+        <div className='my-14 flex justify-center items-center'>
+            <div className=' p-7'>
+                <h2 className='text-3xl text-center font-bold'>LOG IN</h2>
+                <form onSubmit={handleSubmit(handleLogin)}>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"><span className="label-text">Email</span></label>
+                        <input type="text" {...register("email", { required: "Email address is required" })} className="input input-bordered w-full max-w-xs" />
+
+                        {errors.email && <p role="alert" className='text-red-600'>{errors.email?.message}</p>}
+
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"><span className="label-text">Password</span></label>
+                        <input type="password" {...register("password", { required: "Password is required", minLength: { value: 6, message: "Password must be at least 6 characters" } })} className="input input-bordered w-full max-w-xs" />
+
+                        {errors.password && <p role="alert" className='text-red-600'>{errors.password?.message}</p>}
+
+                        <label className="label"><span className="label-text">Forget Password</span></label>
+                    </div>
+                    <input type="submit" className='btn btn-accent w-full' value='Log In' />
+                </form>
+                <p>New to doctors portal? <Link to='/signup' className='text-secondary'>Create new account</Link></p>
+                <div className="divider">OR</div>
+                <button className="btn btn-outline w-full">CONTINUE WITH GOOGLE</button>
+            </div>
         </div>
     );
 };
