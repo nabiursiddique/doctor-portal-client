72e279d4529982d0f401aa979b67ac6697016086
nabiursiddique <nabiursiddique01@gmail.com>
2022-12-30 17:10:58 +0600

    Regex is added to our react hook form

---
+++ src/pages/SignUp/SignUp.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+
+const SignUp = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const handleSignup = data => console.log(data)
+    return (
+        <div className='my-14 flex justify-center items-center'>
+            <div className=' p-7'>
+                <h2 className='text-3xl text-center font-bold'>SIGN UP</h2>
+                <form onSubmit={handleSubmit(handleSignup)}>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"><span className="label-text">Name</span></label>
+                        <input type="text" {...register("name", { required: "Name is required" })} className="input input-bordered w-full max-w-xs" />
+
+                        {errors.name && <p role="alert" className='text-red-600'>{errors.name?.message}</p>}
+
+                    </div>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"><span className="label-text">Email</span></label>
+                        <input type="text" {...register("email", { required: "Email address is required" })} className="input input-bordered w-full max-w-xs" />
+
+                        {errors.email && <p role="alert" className='text-red-600'>{errors.email?.message}</p>}
+
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"><span className="label-text">Password</span></label>
+                        <input type="password" {...register("password", {
+                            required: "Password is required",
+                            minLength: { value: 6, message: "Password must be at least 6 characters" },
+                            pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, message: 'Password must be strong' }
+                        })} className="input input-bordered w-full max-w-xs" />
+
+                        {errors.password && <p role="alert" className='text-red-600'>{errors.password?.message}</p>}
+
+                        <label className="label"><span className="label-text">Forget Password</span></label>
+                    </div>
+                    <input type="submit" className='btn btn-accent w-full' value='SIGN UP' />
+                </form>
+                <p>Already have an account? <Link to='/login' className='text-secondary'>Please LogIn</Link></p>
+                <div className="divider">OR</div>
+                <button className="btn btn-outline w-full">CONTINUE WITH GOOGLE</button>
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
